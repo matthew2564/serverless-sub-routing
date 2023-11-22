a99db20205147eab12180914197e59bad080ecea
Matthew Bell <mbell.dvsa@icloud.com>
2023-11-22 08:17:00 +0000

    Rename constructor name

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -4,10 +4,10 @@ import { UserProvider } from '../providers/UserProvider';
 
 @Service()
 export class UserService {
-	constructor(private userRepository: UserProvider) {}
+	constructor(private userProvider: UserProvider) {}
 
 	async getUserByStaffNumber(staffNumber: string): Promise<void> {
-		const user = await this.userRepository.findUserRecord(staffNumber);
+		const user = await this.userProvider.findUserRecord(staffNumber);
 
 		if (!user) {
 			throw new NotFoundError(`User with staff number ${staffNumber} not found`);

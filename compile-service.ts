38b504066b1935c30b086331aec5776b7f63a6c8
Matthew Bell <mbell.dvsa@icloud.com>
2024-01-14 14:12:01 +0000

    Remove outdir to prevent tsc builds

--- compile-service.ts
+++ compile-service.ts
@@ -15,7 +15,7 @@ const [majorNodeVersion] = readFileSync('.nvmrc', 'utf-8').trim().split('.');
 			external: ['@koa/*', '@babel/*'],
 		});
 
-		console.log('\x1b[32m%s\x1b[0m', '\nArtifact build completed.');
+		console.log('\x1b[36m%s\x1b[0m', '\nProject compiled successfully.');
 	} catch {
 		process.exit(1);
 	}

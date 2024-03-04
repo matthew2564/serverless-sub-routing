c50f2d827db021ddad427f095bd7aad686b4a07e
Matthew Bell <mbell.dvsa@icloud.com>
2024-03-04 15:17:08 +0000

    Add pre-push hook to update OpenAPI spec

--- documentation/generate-openapi-spec.ts
+++ documentation/generate-openapi-spec.ts
@@ -1,38 +1,55 @@
 import 'reflect-metadata';
 import { getMetadataArgsStorage } from 'routing-controllers';
 import { routingControllersToSpec } from 'routing-controllers-openapi';
-import { existsSync, unlinkSync, writeFileSync } from 'fs';
+import fs from 'fs/promises';
+import { name, version } from '../package.json';
 import { app } from '../src';
-
-// Generate the OpenAPI spec
-const spec = routingControllersToSpec(getMetadataArgsStorage(), app, {
-	components: {
-		schemas: {
-			DefectDetails: require('@dvsa/cvs-type-definitions/json-schemas/v1/defect-details/index.json'),
-			RequiredStandards: require('@dvsa/cvs-type-definitions/json-schemas/required-standards/defects/get/index.json'),
-		},
-	},
-	tags: [
-		{
-			name: 'Defects',
-			description: 'API endpoints related to defects',
-		},
-	],
-});
-
-// Convert the spec to a JSON string
-const specString = JSON.stringify(spec, null, 2);
 
 const filePath = './documentation/openapi-spec.json';
 
-// Check if the file exists
-if (existsSync(filePath)) {
-	console.log('Deleting existing OpenAPI spec');
-	// If the file exists, delete it
-	unlinkSync(filePath);
+async function deleteFileIfExists() {
+	try {
+		// Check if the file exists
+		await fs.access(filePath);
+		// If no error is thrown by fs.access, the file exists, so delete it
+		await fs.unlink(filePath);
+	} catch (error) {
+		// An error in fs.access means the file does not exist, so no need to delete
+		if ((error as NodeJS.ErrnoException)?.code !== 'ENOENT') {
+			throw error;
+		}
+		// we want to 'ignore' the ENOENT error so throw if anything else
+	}
 }
 
-// Write the spec to a file
-writeFileSync(filePath, specString);
+(async () => {
+	await deleteFileIfExists();
 
-console.log('OpenAPI spec generated at openapi-spec.json');
+	// Generate the OpenAPI spec
+	const spec = routingControllersToSpec(getMetadataArgsStorage(), app, {
+		info: {
+			title: name,
+			version,
+		},
+		components: {
+			schemas: {
+				DefectDetails: require('@dvsa/cvs-type-definitions/json-schemas/v1/defect-details/index.json'),
+				RequiredStandards: require('@dvsa/cvs-type-definitions/json-schemas/required-standards/defects/get/index.json'),
+			},
+		},
+		tags: [
+			{
+				name: 'Defects',
+				description: 'API endpoints related to defects',
+			},
+		],
+	});
+
+	// Convert the spec to a JSON string
+	const specString = JSON.stringify(spec, null, 2);
+
+	// Write the spec to a file
+	await fs.writeFile(filePath, specString).then(() => {
+		console.log('OpenAPI spec generated at ./documentation/openapi-spec.json');
+	});
+})();
